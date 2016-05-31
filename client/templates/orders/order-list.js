52b2798dba543dfb6dc057e402ada5d22cdae748
Tai Nguyen <tai.tan849@gmail.com>
2016-06-01 00:54:36 +0700

    improve report

--- client/templates/orders/order-list.js
+++ client/templates/orders/order-list.js
@@ -1,13 +1,21 @@
 Template.orderList.helpers({
     totalPrice: function() {
-        var totalPrice = 0;
-        this.products.forEach(function(product) {
-            var price = parseInt(product.price.replace(',', ''));
-            if (!isNaN(price))
-                totalPrice += price;
-        });
+        var totalPrice = calculateTotalPrice(this);
         return totalPrice.toString().split(/(?=(?:\d{3})+$)/).join(",");
-    }
+    },
+    lengthOfStay: function() {
+        var start = moment(this.checkInTime);
+        var end = moment(this.checkOutTime);
+        return end.diff(start, 'minutes');
+    },
+    todayRevenue: function() {
+        var totalRevenue = 0;
+        this.todayOrders.forEach(function(order) {
+            totalRevenue += calculateTotalPrice(order);
+        });
+        return totalRevenue.toString().split(/(?=(?:\d{3})+$)/).join(",");
+    },
+
 });
 
 Template.orderList.events({
@@ -15,7 +23,17 @@ Template.orderList.events({
         e.preventDefault();
         if (confirm("Xóa đơn bán hàng này?")) {
             var orderId = this._id;
-            Orders.remove(orderId);
+            Orders.update(orderId, { $set: { status: "deleted" } });
         }
     }
 });
+
+function calculateTotalPrice(order) {
+    var totalPrice = 0;
+    order.products.forEach(function(product) {
+        var price = parseInt(product.price.replace(',', ''));
+        if (!isNaN(price))
+            totalPrice += price;
+    });
+    return totalPrice;
+}
