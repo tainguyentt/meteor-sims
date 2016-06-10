ca240c21756092e3a4ba9231296597e5395cc9f4
Tai Nguyen <tai.tan849@gmail.com>
2016-06-11 01:28:12 +0700

    improved profit report

--- client/templates/orders/order-list.js
+++ client/templates/orders/order-list.js
@@ -23,41 +23,77 @@ Template.orderList.helpers({
         return numberToCommaFormat(totalRevenue);
     },
     dailyReports: function() {
-        var groupedData = _.groupBy(this.allOrders.fetch(), function(doc) {
-            doc.checkInTime.setMilliseconds(0);
-            doc.checkInTime.setSeconds(0);
-            doc.checkInTime.setMinutes(0);
-            doc.checkInTime.setHours(0);
+        var groupedOrders = _.groupBy(this.allOrders.fetch(), function(doc) {
+            truncateToDay(doc.checkInTime);
             return doc.checkInTime;
         });
-        var result = [];
-        for (var dayKey in groupedData) {
-            var dayRecords = groupedData[dayKey];
+
+        var groupedInvoices = _.groupBy(this.allInvoices.fetch(), function(invoice) {
+            truncateToDay(invoice.createdAt);
+            return invoice.createdAt;
+        });
+
+        var invoiceReports = new Map();
+        for (var dayKey in groupedInvoices) {
+            var dayRecords = groupedInvoices[dayKey];
             var totalPrice = 0;
-            dayRecords.forEach(function(order) {
-                totalPrice += calculateTotalPrice(order);
+            dayRecords.forEach(function(invoice) {
+                var expense = commaToNumberFormat(invoice.cost);
+                if(!isNaN(expense))
+                    totalPrice += expense;
             });
-            result.push({day: dayKey, cost: totalPrice});
+            invoiceReports.set(dayKey, totalPrice);
+        }
+
+        var result = [];
+        for (var dayKey in groupedOrders) {
+            var dayRecords = groupedOrders[dayKey];
+            var totalRevenue = 0;
+            dayRecords.forEach(function(order) {
+                totalRevenue += calculateTotalPrice(order);
+            });
+            var expense = invoiceReports.get(dayKey);
+            if(!expense)
+                expense = 0;
+            var totalProfit = totalRevenue - expense;
+            result.push({ day: dayKey, revenue: totalRevenue, expense: expense, profit: totalProfit });
         }
         return result;
     },
     monthlyReports: function() {
-        var groupedData = _.groupBy(this.allOrders.fetch(), function(doc) {
-            doc.checkInTime.setMilliseconds(0);
-            doc.checkInTime.setSeconds(0);
-            doc.checkInTime.setMinutes(0);
-            doc.checkInTime.setHours(0);
-            doc.checkInTime.setDate(15);
-            return doc.checkInTime;
+        var groupedOrders = _.groupBy(this.allOrders.fetch(), function(order) {
+            truncateToMonth(order.checkInTime);
+            return order.checkInTime;
         });
-        var result = [];
-        for (var monthKey in groupedData) {
-            var monthRecords = groupedData[monthKey];
+
+        var groupedInvoices = _.groupBy(this.allInvoices.fetch(), function(invoice) {
+            truncateToMonth(invoice.createdAt);
+            return invoice.createdAt;
+        });
+        var invoiceReports = new Map();
+        for (var monthKey in groupedInvoices) {
+            var monthRecords = groupedInvoices[monthKey];
             var totalPrice = 0;
-            monthRecords.forEach(function(order) {
-                totalPrice += calculateTotalPrice(order);
+            monthRecords.forEach(function(invoice) {
+                var expense = commaToNumberFormat(invoice.cost);
+                if(!isNaN(expense))
+                    totalPrice += expense;
             });
-            result.push({month: monthKey, cost: totalPrice});
+            invoiceReports.set(monthKey, totalPrice);
+        }
+
+        var result = [];
+        for (var monthKey in groupedOrders) {
+            var monthRecords = groupedOrders[monthKey];
+            var totalRevenue = 0;
+            monthRecords.forEach(function(order) {
+                totalRevenue += calculateTotalPrice(order);
+            });
+            var expense = invoiceReports.get(monthKey);
+            if(!expense)
+                expense = 0;
+            var totalProfit = totalRevenue - expense;
+            result.push({ month: monthKey, revenue: totalRevenue, expense: expense, profit: totalProfit });
         }
         return result;
     }
@@ -81,10 +117,25 @@ function calculateTotalPrice(order) {
             totalPrice += price;
     });
     var discount;
-    if(order.discountAmount)  {
+    if (order.discountAmount) {
         discount = commaToNumberFormat(order.discountAmount);
         if (!isNaN(discount))
-        totalPrice -= discount;
+            totalPrice -= discount;
     }
     return totalPrice;
+}
+
+function truncateToMonth(time) {
+    time.setMilliseconds(0);
+    time.setSeconds(0);
+    time.setMinutes(0);
+    time.setHours(0);
+    time.setDate(15);
+}
+
+function truncateToDay(time){
+    time.setMilliseconds(0);
+    time.setSeconds(0);
+    time.setMinutes(0);
+    time.setHours(0);
 }
